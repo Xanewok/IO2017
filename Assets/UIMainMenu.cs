7468eeba43153e7f1c3e3fc215d5fd07a81452a4
Igor Matuszewski <Xanewok@gmail.com>
2017-03-17 18:35:25 +0100

    simple scene layout + basic Main Menu

---
+++ Assets/UIMainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIMainMenu : MonoBehaviour
+{
+	// Use this for initialization
+	void Start()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+		
+	}
+
+    public void Play()
+    {
+        SceneManager.LoadScene("default");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
