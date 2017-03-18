bb38c8b21132c9dc2b7e7a417c02383bd1b2289e
Igor Matuszewski <Xanewok@gmail.com>
2017-03-18 15:51:46 +0100

    new folders for assets, camera follow script, simple player prefab

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    Transform myTransform;
+
+    public Transform followTransform;
+    [Range(0.0f, 1.0f)]
+    public float followSpeed = 0.5f;
+
+    public void SetFollowTarget(GameObject obj)
+    {
+        followTransform = obj == null ? null : obj.GetComponent<Transform>();
+    }
+
+    void Awake()
+    {
+        myTransform = GetComponent<Transform>();
+    }
+
+    void Start()
+    {
+        if (followTransform == null)
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            SetFollowTarget(player);
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (followTransform == null)
+            return;
+
+        Vector3 offset = (followTransform.position - myTransform.position);
+        offset.y = 0; // Follow only in XZ
+
+        myTransform.position += (offset * followSpeed);
+    }
+}
