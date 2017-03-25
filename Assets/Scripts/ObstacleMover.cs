5d0514d37e6d01c940cfd4314ae521a393e3807f
Igor Matuszewski <Xanewok@gmail.com>
2017-03-25 12:55:22 +0100

    simple moving obstacle template

---
+++ Assets/Scripts/ObstacleMover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ObstacleMover : MonoBehaviour
+{
+    public Vector3 movementVector;
+
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	private void FixedUpdate()
+    {
+        Vector3 movement = movementVector * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + movement);
+	}
+}
