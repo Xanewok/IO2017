7319df2c6343a19298802aa0f34dd38574b2c119
Igor Matuszewski <Xanewok@gmail.com>
2017-04-02 01:19:30 +0200

    add mouse support toggle button to pause menu

---
+++ Assets/Scripts/UI/OptionsMouse.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsMouse : MonoBehaviour
+{
+    private PlayerControlls playerControls = null;
+    private Text text;
+
+    private void Awake()
+    {
+        text = GetComponentInChildren<Text>();
+    }
+
+    void Start()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        playerControls = player != null ? player.GetComponentInChildren<PlayerControlls>() : null;
+
+        if (playerControls)
+            UpdateText(playerControls.supportMouse);
+    }
+
+    private void OnEnable()
+    {
+        if (playerControls)
+            UpdateText(playerControls.supportMouse);
+    }
+
+    public void ToggleMouseSupport()
+    {
+        playerControls.supportMouse = !playerControls.supportMouse;
+        UpdateText(playerControls.supportMouse);
+    }
+
+    void UpdateText(bool enabled)
+    {
+        text.text = "Mouse: " + (enabled ? "On" : "Off");
+    }
+}
