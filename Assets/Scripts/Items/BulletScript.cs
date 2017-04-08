be56d689a9325109c1c3f4809d9c584c6cfca94e
Mryle <mrylemail@gmail.com>
2017-04-08 12:18:37 +0200

    Simple gun prefab & Script
    Might be not too quick to run, but it should be accurate.

---
+++ Assets/Scripts/Items/BulletScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour {
+
+    public float damage = 10f;
+    public int damageType = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+        Status stat = collision.gameObject.GetComponent<Status>();
+        if (stat != null)
+        {
+            stat.hurt(damage, damageType);
+        }
+    }
+
+
+}
