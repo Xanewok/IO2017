25189e69f3500a850da9a704038d93197aa31251
Mryle <mrylemail@gmail.com>
2017-04-11 11:50:13 +0200

    Adding simple bonus - random weapon.

---
+++ Assets/Scripts/Items/InstantiateBonus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateBonus : BonusObject {
+
+    [Tooltip("List of thing that can be instantiated on use")]
+    public GameObject[] possibleThings;
+
+    protected override void OnUse()
+    {
+        if (possibleThings.Length > 0)
+        {
+            System.Random rng= new System.Random();
+            int chosenIndex = rng.Next(possibleThings.Length);
+            GameObject toInstantiate = possibleThings[chosenIndex];
+            Instantiate(toInstantiate, transform.position, transform.rotation);
+        }
+    }
+}
