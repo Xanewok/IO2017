229a1bee373a01a469086ae9daab5c3b114079c0
Mryle <mrylemail@gmail.com>
2017-04-11 11:48:31 +0200

    Adding base class for bonuses.

---
+++ Assets/Scripts/Items/BonusObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BonusObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    protected abstract void OnUse();
+
+    void OnTriggerEnter(Collider other)
+    {
+        PlayerControlls player = other.gameObject.GetComponent<PlayerControlls>();
+        if (player != null)
+        {
+            this.OnUse();
+            Destroy(gameObject);
+        }
+    }
+}
