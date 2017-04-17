5aa5261d65e4dc2374eecbeaad89d7faa3944c58
Mryle <mrylemail@gmail.com>
2017-04-17 18:47:04 +0200

    Adding pointles text-showing bonus. (For events?)

---
+++ Assets/Scripts/Items/Bonuses/TextspawningBonus.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextspawningBonus : BonusObject {
+
+    public TextSpawner spawner;
+    public String textToSpawn = "Random text";
+
+    protected override void OnUse()
+    {
+        spawner.spawnText(textToSpawn);
+    }
+
+    // Use this for initialization
+    void Start () {
+		if (spawner == null)
+        {
+            spawner = gameObject.GetComponent<TextSpawner>();
+        }
+	}
+}
