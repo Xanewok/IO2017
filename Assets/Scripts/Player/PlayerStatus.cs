63ea525d5fcbf42991139c2bd42510c9a87368e5
Mryle <mrylemail@gmail.com>
2017-04-22 21:47:21 +0200

    Cleaning auras
    As we have decided to use more direct approach, aura idea might no
    longer be useful.
    On the other hand, adding dash vector, for dashing.

--- Assets/Scripts/Player/PlayerStatus.cs
+++ Assets/Scripts/Player/PlayerStatus.cs
@@ -8,22 +8,7 @@ using UnityEngine;
 /// </summary>
 public class PlayerStatus : Status
 {
-    /// <summary>
-    /// Below what health should hope aura be active
-    /// </summary>
-    [Tooltip("Below what health should hope aura be active")]
-    public float healthThreshold = 25f;
-
     public override void onStart()
     {
-    }
-
-    protected override void fillAura(int aura)
-    {
-        if (aura == Status.Hope)
-        {
-            simpleAuras[aura] = new HopeAura(this, healthThreshold);
-        }
-        else base.fillAura(aura);
     }
 }
