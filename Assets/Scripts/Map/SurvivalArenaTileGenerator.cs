b8cbb780586a0742e14058bdb0b6799dc2ad401f
Igor Matuszewski <Xanewok@gmail.com>
2017-04-27 20:05:43 +0200

    stub survival arena tile generator

---
+++ Assets/Scripts/Map/SurvivalArenaTileGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArenaTileGenerator : MonoBehaviour, ITileGenerator
+{
+    void ITileGenerator.BuildAfterNavMesh(Vector3 origin)
+    {
+        throw new NotImplementedException();
+    }
+
+    void ITileGenerator.BuildBeforeNavMesh(Vector3 origin)
+    {
+        throw new NotImplementedException();
+    }
+
+    void ITileGenerator.Clear()
+    {
+        throw new NotImplementedException();
+    }
+
+    GameObject[] ITileGenerator.GetSpawnedTiles()
+    {
+        throw new NotImplementedException();
+    }
+}
