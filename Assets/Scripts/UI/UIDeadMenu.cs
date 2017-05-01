7d4379fc65f7a4ee49124fd3703d69856f1808c4
Igor Matuszewski <Xanewok@gmail.com>
2017-05-01 19:35:09 +0200

    background fade for dead screen

---
+++ Assets/Scripts/UI/UIDeadMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class UIDeadMenu : MonoBehaviour
+{
+    public float fadeDelay = 0.5f;
+    public ImageFader backgroundFader;
+
+    void Start()
+    {
+        StartCoroutine(BackgroundFade());
+    }
+
+    IEnumerator BackgroundFade()
+    {
+        yield return new WaitForSecondsRealtime(fadeDelay);
+        backgroundFader.Fade();
+    }
+}
