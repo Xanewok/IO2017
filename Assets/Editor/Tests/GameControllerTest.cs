b3e429e5af86955e362cc1022ce5a254feb1e15c
Igor Matuszewski <Xanewok@gmail.com>
2017-05-02 00:00:38 +0200

    introduced notion of game controller + simple test

---
+++ Assets/Editor/Tests/GameControllerTest.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+using NUnit.Framework;
+using System;
+using System.IO;
+using System.Collections;
+using System.Linq;
+
+public class GameControllerTest
+{
+    public static GameObject CreateGameController()
+    {
+        var go = new GameObject();
+        go.AddComponent<GameController>();
+        return go;
+    }
+
+    [Test]
+    public void PointsToValidInitialScene()
+    {
+        var gameObject = CreateGameController();
+        var gameController = gameObject.GetComponent<GameController>();
+
+        Assert.IsTrue(EditorBuildSettings.scenes.Any(scene =>
+        {
+            var sceneName = Path.GetFileNameWithoutExtension(scene.path);
+            return scene.enabled && sceneName.Equals(gameController.initialScene);
+        }));
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public string initialScene = "Main_Menu";
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        SceneManager.LoadScene(initialScene);
+    }
+}
