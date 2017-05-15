dd503ce130b1ae7f88c82793f37013ecb6ecae73
Igor Matuszewski <Xanewok@gmail.com>
2017-05-15 22:18:33 +0200

    Revamp camera follow script to allow for arbitrary camera rotation

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    Transform myTransform;
-
     public Transform followTransform;
     [Range(0.0f, 1.0f)]
-    public float followSpeed = 0.5f;
+    public float followSpeed = 0.05f;
+    [Range(0.0f, 50.0f)]
+    public float distance = 10.0f;
+    public Vector3 rotation = Vector3.zero;
+
+    private Transform myTransform;
 
     public void SetFollowTarget(GameObject obj)
     {
@@ -34,9 +37,14 @@ public class CameraFollow : MonoBehaviour
         if (followTransform == null)
             return;
 
-        Vector3 offset = (followTransform.position - myTransform.position);
-        offset.y = 0; // Follow only in XZ
+        // Default position camera is directly above (up) the follow target
+        Vector3 toFollowTransform = -(Quaternion.Euler(rotation) * followTransform.up) * distance;
 
+        Vector3 targetPosition = followTransform.position - toFollowTransform;
+        Quaternion targetRotation = Quaternion.LookRotation(toFollowTransform);
+
+        Vector3 offset = (targetPosition - myTransform.position);
         myTransform.position += (offset * followSpeed * Time.timeScale);
+        myTransform.rotation = targetRotation;
     }
 }
