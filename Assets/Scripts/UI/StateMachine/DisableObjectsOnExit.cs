293e94331b71c2215f411d739406374ffa9bb9d0
Igor Matuszewski <Xanewok@gmail.com>
2017-05-17 01:46:51 +0200

    Revamp Main Menu state machine

---
+++ Assets/Scripts/UI/StateMachine/DisableObjectsOnExit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class DisableObjectsOnExit : StateMachineBehaviour
+{
+    public string[] objectNames;
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        var transforms = animator.gameObject.GetComponentsInChildren<Transform>(true)
+            .Where(transform => objectNames.Contains(transform.name));
+
+        foreach (var transform in transforms)
+            transform.gameObject.SetActive(false);
+    }
+}
