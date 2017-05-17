38a743bb699d0ad24b4bb933a6736a2275568a90
Igor Matuszewski <Xanewok@gmail.com>
2017-05-17 22:45:41 +0200

    Make UICameraPreset use new generic option picker implementation

---
+++ Assets/Scripts/UI/UICameraPresetPicker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using YAGTSS.UI;
+using YAGTSS.Utility;
+
+public class UICameraPresetPicker : SelectedOptionPicker<CameraSettings.CameraType>
+{
+    protected override void Awake()
+    {
+        base.Awake();
+
+        var values = EnumHelper.GetValueNameMappingPretty<CameraSettings.CameraType>();
+        InitializeWithValues(values);
+
+        var activeCameraType = GameController.Instance.gameSettings.cameraSettings.currentType;
+        MarkSelectedOption(activeCameraType);
+    }
+
+    protected override void OnOptionSelected(KeyValuePair<CameraSettings.CameraType, string> value)
+    {
+        GameController.Instance.gameSettings.cameraSettings.currentType = value.Key;
+        MarkSelectedOption(value.Key);
+    }
+}
