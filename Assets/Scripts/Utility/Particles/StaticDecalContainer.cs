cf89ad0a275a0677ae66c599cd1645eee4e1fe18
Igor Matuszewski <Xanewok@gmail.com>
2017-05-28 19:30:36 +0200

    Reset decal ParticleSystem on level load

--- Assets/Scripts/Utility/Particles/StaticDecalContainer.cs
+++ Assets/Scripts/Utility/Particles/StaticDecalContainer.cs
@@ -1,53 +1,69 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class StaticDecalContainer : DecalContainer {
-	[Tooltip("Maximum number of particles this container can show.")]
-	public int maxDecals = 100;
+public class StaticDecalContainer : DecalContainer
+{
+    [Tooltip("Maximum number of particles this container can show.")]
+    public int maxDecals = 100;
 
-	public ParticleSystem decalParticleSystem;
-	private int particleDecalDataIndex;
-	private ParticleDecalData[] particleData;
-	private ParticleSystem.Particle[] particles;
+    public ParticleSystem decalParticleSystem;
+    private int particleDecalDataIndex;
+    private ParticleDecalData[] particleData;
+    private ParticleSystem.Particle[] particles;
 
-	// Use this for initialization
-	void Start () {
-		if (decalParticleSystem == null)
-			decalParticleSystem = gameObject.GetComponent<ParticleSystem> ();
-		particles = new ParticleSystem.Particle[maxDecals];
-		particleData = new ParticleDecalData[maxDecals];
-		for (int i = 0; i < maxDecals; i++) 
-		{
-			particleData [i] = new ParticleDecalData ();    
-		}
-	}
+    void Awake()
+    {
+        if (decalParticleSystem == null)
+            decalParticleSystem = gameObject.GetComponent<ParticleSystem>();
+   
+        particles = new ParticleSystem.Particle[maxDecals];
+        particleData = new ParticleDecalData[maxDecals];
 
-	public override void AddDecal(Vector3 position, float size, Vector3 rotationEuler, Color color) {
-		if (particleDecalDataIndex >= maxDecals) 
-		{
-			particleDecalDataIndex = 0;
-		}
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
 
-		particleData [particleDecalDataIndex].position = position;
-		particleData [particleDecalDataIndex].rotation = rotationEuler;
-		particleData [particleDecalDataIndex].size = size;
-		particleData [particleDecalDataIndex].color = color;
+    public override void AddDecal(Vector3 position, float size, Vector3 rotationEuler, Color color)
+    {
+        if (particleDecalDataIndex >= maxDecals)
+        {
+            particleDecalDataIndex = 0;
+        }
 
-		particleDecalDataIndex++;
-		DisplayParticles ();
-	}
+        particleData[particleDecalDataIndex].position = position;
+        particleData[particleDecalDataIndex].rotation = rotationEuler;
+        particleData[particleDecalDataIndex].size = size;
+        particleData[particleDecalDataIndex].color = color;
 
-	void DisplayParticles()
-	{
-		for (int i = 0; i < particleData.Length; i++) 
-		{
-			particles [i].position = particleData [i].position;
-			particles [i].rotation3D = particleData [i].rotation;
-			particles [i].startSize = particleData [i].size;
-			particles [i].startColor = particleData [i].color;
-		}
+        particleDecalDataIndex++;
+        DisplayParticles();
+    }
 
-		decalParticleSystem.SetParticles (particles, particles.Length);
-	}
+    void Reset()
+    {
+        particleDecalDataIndex = 0;
+        decalParticleSystem.SetParticles(null, 0);
+    }
+
+    void DisplayParticles()
+    {
+        for (int i = 0; i < particleData.Length; i++)
+        {
+            particles[i].position = particleData[i].position;
+            particles[i].rotation3D = particleData[i].rotation;
+            particles[i].startSize = particleData[i].size;
+            particles[i].startColor = particleData[i].color;
+        }
+
+        decalParticleSystem.SetParticles(particles, particles.Length);
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (mode == LoadSceneMode.Single)
+        {
+            Reset();
+        }
+    }
 }
