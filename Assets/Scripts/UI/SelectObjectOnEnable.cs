916d54add2217f5ae22e43d6ca8cd9c2bd029309
Igor Matuszewski <Xanewok@gmail.com>
2017-05-28 20:52:25 +0200

    Fix Main Menu navigation for gamepads

---
+++ Assets/Scripts/UI/SelectObjectOnEnable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectObjectOnEnable : MonoBehaviour
+{
+    public Selectable selectable;
+
+    private void OnEnable()
+    {
+        StartCoroutine(DoSelect());
+    }
+
+    IEnumerator DoSelect()
+    {
+        // Wait a single frame as sometimes selectable is logically selected,
+        // but it doesn't visually refresh (it's done too fast?) at all
+        yield return null;
+
+        selectable.Select();
+    }
+}
