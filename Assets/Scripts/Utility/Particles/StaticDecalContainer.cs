dbf34184b8cc93b79ac675493d311aaa5af163bf
Igor Matuszewski <xanewok@gmail.com>
2017-05-30 22:41:24 +0200

    Actually fix particle blood reset

--- Assets/Scripts/Utility/Particles/StaticDecalContainer.cs
+++ Assets/Scripts/Utility/Particles/StaticDecalContainer.cs
@@ -43,7 +43,7 @@ public class StaticDecalContainer : DecalContainer
     void Reset()
     {
         particleDecalDataIndex = 0;
-        particles = new ParticleSystem.Particle[maxDecals];
+		particleData = new ParticleDecalData[maxDecals];
         decalParticleSystem.SetParticles(null, 0);
     }
 
