8f2bc8da8d635a647195b103c61de7d748b44b24
Mryle <mrylemail@gmail.com>
2017-05-30 09:23:45 +0200

    DropOnDead

---
+++ Assets/Scripts/Status/SpawnOnDead.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnOnDead : MonoBehaviour {
+
+	public GameObject[] spawnable;
+
+	void Start () {
+		gameObject.GetComponent<Status>().healthChanged += OnEnemyHealthChanged;
+	}
+
+	void OnEnemyHealthChanged(GameObject enemy, float health)
+	{
+		if (health <= 0) {
+			int i = Random.range (0, spawnable.Length ());
+			if (spawnable [i] != null) {
+				Instantiate (spawnable [i], transform.position);
+			}
+		}
+	}
+}
