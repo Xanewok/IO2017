7128e11167ea17b3478c1127cd38fd67f67d0e3e
Mryle <mrylemail@gmail.com>
2017-06-02 13:00:49 +0200

    Making Inventory canvas into world space.
    (Having some problems with moving camera.)

---
+++ Assets/Scripts/Utility/ConstantTransform.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantTransform : MonoBehaviour {
+
+	public bool constantLocalPosition;
+	public Vector3 localPosition;
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		if (constantLocalPosition)
+			transform.localPosition = localPosition;
+	}
+}
